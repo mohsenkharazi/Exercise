868d761daea787e6007206533f3ac9b1e5c724ca
Mohsen Kharazi <mohsenkharazi@yahoo.com>
2021-01-17 20:31:10 +0800

    async

---
+++ Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        [HttpGet("DotNetCount")]
+        public async Task<int> GetDotNetCount()
+        {
+            var html = await _httpClient.GetStringAsync("https://dotnetfoundation.org");
+
+            return Regex.Matches(html, @".net").Count;
+        }        
+    }
+}
