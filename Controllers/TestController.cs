044781ccce31287694f873147090e02b812d7a2d
Mohsen Kharazi <mohsenkharazi@yahoo.com>
2021-01-19 22:40:19 +0800

    Excersize Swagger

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,12 +10,32 @@ using System.Threading.Tasks;
 namespace Exercise.Controllers
 {
     [ApiController]
+    [Produces("application/json")]
     [Route("[controller]")]
     public class TestController : ControllerBase
     {
         private readonly HttpClient _httpClient = new HttpClient();
 
+        /// <summary>
+        /// to count number of .Net substring accurance
+        /// </summary>
+        /// <returns>an integer showing number of .net accurance</returns>
+        /// <response code="200">Returns count of accurance</response>
+        /// <response code="400">If the item is null</response> 
+        /// /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "id": 1,
+        ///        "name": "Item1",
+        ///        "isComplete": true
+        ///     }
+        ///
+        /// </remarks>
         [HttpGet("DotNetCount")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<int> GetDotNetCount()
         {
             var html = await _httpClient.GetStringAsync("https://dotnetfoundation.org");
--- Startup.cs
+++ Startup.cs
@@ -6,9 +6,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Exercise
@@ -26,6 +29,34 @@ namespace Exercise
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +66,17 @@ namespace Exercise
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger(options => 
+                { 
+                    options.SerializeAsV2 = true; 
+                }) ; 
+
+            app.UseSwaggerUI(options => 
+                { 
+                    options.SwaggerEndpoint(@"/swagger/v1/swagger.json", "API version one");
+                    options.RoutePrefix = string.Empty;
+                });
 
             app.UseHttpsRedirection();
 
